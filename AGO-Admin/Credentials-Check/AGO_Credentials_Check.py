ee5f08b6819671c9dc61c5eb80be7590702ed7dc
Michael Dykes <40680569+MichaelDykesBC@users.noreply.github.com>
2024-02-13 14:49:53 -0800

    Updates

--- AGO-Admin/Credentials-Check/AGO_Credentials_Check.py
+++ AGO-Admin/Credentials-Check/AGO_Credentials_Check.py
@@ -23,13 +23,11 @@ except:
     _log.info(f"bier module imported")
 
 def Send_Teams_WebHook_Message():
-        url = conf['BIER_Teams_Webhook']
-        payload = {"text": f"Warning - Automated attempt to connect to GeoHub from '{portal_username}' account failed. Credentials may have been altered."}
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        _log.info(response.text.encode('utf8'))
-
-
+    url = os.environ['BIER_TEAMS_WEBHOOK']
+    payload = {"text": f"Warning - Automated attempt to connect to GeoHub from '{os.environ['AGO_USER']}' account failed. Credentials may have been altered."}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    _log.info(response.text.encode('utf8'))
 
 def main():
     '''Run code'''
@@ -38,44 +36,17 @@ def main():
     try:
         conf = bier.Find_Config_File(__file__)
         AGO_Portal_URL = conf['AGO_PORTAL_URL']
-        AVALANCHEFORECAST_ITEMID = conf['AVALANCHEFORECAST_ITEMID']
     except:
         AGO_Portal_URL = os.environ['AGO_PORTAL_URL']
-        AVALANCHEFORECAST_ITEMID = os.environ['AVALANCHEFORECAST_ITEMID']
 
-    AGO = bier.AGO_Connection(AGO_Portal_URL)
-
-    avalanche_geometry_url = r"https://api.avalanche.ca/forecasts/en/areas"
-    avalanche_geometry_data = bier.Connect_to_Website_or_API_JSON(avalanche_geometry_url)
-    avalanche_attribute_url = r"https://api.avalanche.ca/forecasts/en/products"
-    avalanche_attribute_data = bier.Connect_to_Website_or_API_JSON(avalanche_attribute_url,encode=True)
-
-    avalanche_dict = Format_Avalanche_Forecast_Data(avalanche_geometry_data,avalanche_attribute_data)
-    AvalancheForecast_item = bier.AGO_Item(AGO,AVALANCHEFORECAST_ITEMID)
-    Update_Avalanche_Forecast_AGO(avalanche_dict,AvalancheForecast_item)
+    try:
+        AGO = bier.AGO_Connection(AGO_Portal_URL)
+    except:
+        Send_Teams_WebHook_Message()
 
     AGO.disconnect()
     _log.info("**Script completed**")
 
 # main function of script (if script is not imported as a module)
-if __name__ == "__main__":
-    main()
-
-
-
-def main():
-    '''Run code (if executed as script)'''
-    Set_Logging_Level(logging_level)
-    Find_Config_File()
-    try:
-        Create_AGO_Connection()
-    except:
-        url = conf['BIER_Teams_Webhook']
-
-        payload = {"text": f"Warning - Automated attempt to connect to GeoHub from '{portal_username}' account failed. Credentials may have been altered."}
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        log.info(response.text.encode('utf8'))
-
 if __name__ == "__main__":
     main()
