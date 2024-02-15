40ca0699f2de548f19ee6aa29839598e47c1bfde
Michael Dykes <40680569+MichaelDykesBC@users.noreply.github.com>
2024-02-15 10:25:37 -0800

    Update BCHydro_Outages.py

--- BCHydro-Outages/BCHydro_Outages.py
+++ BCHydro-Outages/BCHydro_Outages.py
@@ -10,22 +10,23 @@ Purpose: Grab BC Hydro Web Content (from https://www.bchydro.com/power-outages/a
 # Import libraries/modules
 import os, sys, datetime, logging
 from arcgis import geometry, features
+from dotenv import load_dotenv
 
 # set script logger
 _log = logging.getLogger(f"{os.path.basename(os.path.splitext(__file__)[0])}")
 
-# append path to BIER module for import
-if 'JENKINS_URL' in os.environ:
-    sys.path.append(sys.argv[1])
-else:
-    sys.path.append(".")
-try:
-    import bier
-    _log.info(f"bier module imported")
-except:
-    sys.path.append(sys.argv[1])
-    import bier
-    _log.info(f"bier module imported")
+def import_environment_variables_from_file():
+    '''
+    Import Environment Variables from File (if necessary)
+    '''
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    environment_file_path = os.path.join(script_dir, 'environment.env')
+
+    try:
+        load_dotenv(dotenv_path=environment_file_path)
+        _log.info(f"Environment Variables Imported from File Successfully")
+    except:
+        _log.info(f"Environment Variables Imported Not Imported from File")
 
 def Update_BCHydro_Outages_AGO(bchydro_data,bchydro_item):
     '''Clear existing features, and append new features to the BC Hydro Outages hosted feature layer in AGO'''
@@ -68,15 +69,31 @@ def Update_BCHydro_Outages_AGO(bchydro_data,bchydro_item):
 
 def main():
     '''Run code (if executed as script)'''
+    import_environment_variables_from_file()
+
+    # import bier module
+    try:
+        import bier
+        _log.info(f"bier module imported")
+    except:
+        sys.path.append(".")
+        sys.path.append(os.environ["BIER_PATH"])
+        import bier
+        _log.info(f"bier module imported")
+
     bier.Set_Logging_Level(logging.DEBUG)
-    conf = bier.Find_Config_File(__file__)
-    AGO = bier.AGO_Connection(conf["AGO_Portal_URL"])
+
+    AGO_Portal_URL = os.environ["AGO_PORTAL_URL"]
+    HydroOutages_ItemID = os.environ["HYDROOUTAGES_ITEMID"]
+    HydroOutagesLFN_ItemID = os.environ["HYDROOUTAGES_LFN_ITEMID"]
+
+    AGO = bier.AGO_Connection(AGO_Portal_URL)
     bchydro_outages_url = r"https://www.bchydro.com/power-outages/app/outages-map-data.json"
     bchydro_data = bier.Connect_to_Website_or_API_JSON(bchydro_outages_url)
 
-    HydroOutages_item = bier.AGO_Item(AGO,conf["HydroOutages_ItemID"])
+    HydroOutages_item = bier.AGO_Item(AGO,HydroOutages_ItemID)
     Update_BCHydro_Outages_AGO(bchydro_data,HydroOutages_item)
-    HydroOutagesLFN_item = bier.AGO_Item(AGO,conf["HydroOutagesLFN_ItemID"])
+    HydroOutagesLFN_item = bier.AGO_Item(AGO,HydroOutagesLFN_ItemID)
     Update_BCHydro_Outages_AGO(bchydro_data,HydroOutagesLFN_item)
     AGO.disconnect()
     _log.info("**Script completed**")
