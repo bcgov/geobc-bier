e1b41b8e4c770bf569b79a66d9eb1b33f90fdc23
Michael Dykes <40680569+MichaelDykesBC@users.noreply.github.com>
2025-02-10 12:07:30 -0800

    Update to version 2.0.0

--- bier/setup.py
+++ bier/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='bier',
-        version='1.0.1',
+        version='2.0.0',
         description='Custom python functions for GeoBC Business Innovation and Emergency Response',
         url='https://github.com/bcgov/geobc-bier/tree/main/bier',
         author='Michael Dykes',
         author_email='Michael.Dykes@gov.bc.ca',
-        license='Aparche',
+        license='Apache-2.0',
         packages=['bier'])
