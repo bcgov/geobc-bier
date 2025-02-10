3be0043daf37843556b91695849b2cb9d7392df7
Michael Dykes <40680569+MichaelDykesBC@users.noreply.github.com>
2025-02-10 15:16:21 -0800

    Update general_util.py

--- bier/bier/general_util.py
+++ bier/bier/general_util.py
@@ -1,16 +1,11 @@
 """
-bier function library (bier.py)
+bier general function library (bier.py)
 Author: Michael Dykes (michael.dykes@gov.bc.ca)
-Created: May 29, 2023
+Created: February 10, 2025
 
 Description:
         Contains common classes and functions used by the Business Innovation and Emergency Response section at GeoBC
-        for GIS data processing and interaction with ArcGIS Online (AGO) and other GIS tools.
-
-Dependencies:
-    - arcgis
-    - minio
-    - tenacity
+        for general python script functionality.
 """
 
 import logging
@@ -24,6 +19,9 @@ logging.basicConfig(
 )
 _log = logging.getLogger(os.path.basename(os.path.splitext(__file__)[0]))
 
+# Cache for config file
+_config_cache = None
+
 # Generic Python Utilities
 def find_config_file(script_file_path: str) -> dict:
     """
@@ -36,20 +34,26 @@ def find_config_file(script_file_path: str) -> dict:
         dict or None: The parsed JSON configuration dictionary if found,
                       otherwise None.
     """
+    global _config_cache
+    if _config_cache is not None:
+        _log.info("Returning cached config.")
+        return _config_cache
+
     config_file_path: str = os.path.join(
         os.path.dirname(script_file_path), "config.json"
     )
     try:
         with open(config_file_path, encoding="utf-8") as json_conf:
             config_dict: dict = json.load(json_conf)
-        _log.info("Config file found")
+        _config_cache = config_dict
+        _log.info(f"Config file found at {config_file_path}")
         return config_dict
     except FileNotFoundError:
-        _log.warning("Config file not found.")
+        _log.warning(f"Config file not found at {config_file_path}.")
     except json.JSONDecodeError as e:
-        _log.warning(f"Invalid JSON format: {e}")
+        _log.warning(f"Invalid JSON format in config file: {e}")
     except Exception as e:
-        _log.warning(f"Unexpected error: {e}")
+        _log.warning(f"Unexpected error loading config file: {e}")
     return None
 
 def get_env_variable(var_name: str, default: str = "") -> str:
@@ -63,4 +67,9 @@ def get_env_variable(var_name: str, default: str = "") -> str:
     Returns:
         str: The value of the environment variable or the default value.
     """
-    return os.getenv(var_name, default)
+    value = os.getenv(var_name, default)
+    if value == default:
+        _log.warning(f"Environment variable {var_name} not found. Using default value.")
+    else:
+        _log.info(f"Environment variable {var_name} found with value: {value}")
+    return value
